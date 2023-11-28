3f4f4b3b0b812b9d1ee204217c891da0b1458c3a
Mohammed Siddiqui <abztmars@gmail.com>
2023-11-28 14:07:12 -0500

    Initial Commit

---
+++ CollegeDelivery/CollegeDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollegeDeliveryApp.swift
+//  CollegeDelivery
+//
+//  Created by Mohammed Siddiqui on 2023-11-28.
+//
+
+import SwiftUI
+
+@main
+struct CollegeDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CollegeDelivery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CollegeDelivery
+//
+//  Created by Mohammed Siddiqui on 2023-11-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
