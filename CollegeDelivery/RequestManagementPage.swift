b897660a423beb85536345cd7fd80e432044e634
jpveloso <jpveloso@hotmail.ca>
2023-11-29 02:43:19 -0500

    Basic views created

---
+++ CollegeDelivery/RequestManagementPage.swift
@@ -0,0 +1,18 @@
+//
+//  RequestManagementPage.swift
+//  CollegeDelivery
+//
+//  Created by Paolo Veloso on 2023-11-29.
+//
+
+import SwiftUI
+
+struct RequestManagementPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RequestManagementPage()
+}
