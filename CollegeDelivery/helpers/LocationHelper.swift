37b2cb0ad5b96d43fe105d17e3a1fe73a7ffc9f5
NickFabek <nickfabek@gmail.com>
2023-12-11 19:08:13 -0500

    Added extra mapping functionality with UI to move the map across to the various campuses with reverse geocoding.

--- CollegeDelivery/helpers/LocationHelper.swift
+++ CollegeDelivery/helpers/LocationHelper.swift
@@ -10,14 +10,25 @@ import CoreLocation
 
 class LocationHelper : NSObject, ObservableObject, CLLocationManagerDelegate{
     @Published var currentLocation : CLLocation?
+    @Published var targetLocation: CLLocation?
     private let locationManager = CLLocationManager()
     private let geoCoder = CLGeocoder()
+   
+    
     
     override init(){
         super.init()
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         self.locationManager.delegate = self
     }
+   
+    
+    
+    func setTargetLocationTo(address: String) {
+        performForwardGeocoding(address: address)
+    }
+   
+    
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         switch manager.authorizationStatus{
@@ -41,6 +52,8 @@ class LocationHelper : NSObject, ObservableObject, CLLocationManagerDelegate{
                 manager.requestWhenInUseAuthorization()
         }
     }
+   
+    
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if (locations.isEmpty){
@@ -57,7 +70,66 @@ class LocationHelper : NSObject, ObservableObject, CLLocationManagerDelegate{
         }
     }
     
+    
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(#function, "Unable to receive location changes due to error : \(error)")
     }
+   
+    
+    
+    func performForwardGeocoding(address : String) {
+        self.geoCoder.geocodeAddressString(address, completionHandler: { (placemarks, error) in
+            if (error == nil) {
+                if (placemarks != nil){
+                    if let location = placemarks!.first{
+                        print(#function, "obtained location : \(location)")
+                        self.targetLocation = location.location
+                    }
+                } else {
+                    print(#function, "No matching coordinates for given address")
+                }
+            } else {
+                print(#function, "Coordinates for given address is not available : \(String(describing: error))")
+            }
+        })
+    }
+
+    
+    
+    func performReverseGeocoding(location : CLLocation, completionHandler : @escaping(String?, NSError?) -> Void) {
+        self.geoCoder.reverseGeocodeLocation(location, completionHandler: { (placemarkList, error) in
+            if (error != nil){
+                print(#function, "Couldn't perform reverse geocoding successfully due to error : \(String(describing: error))")
+                completionHandler(nil, error as? NSError)
+            } else {
+                if let addresses = placemarkList {
+                    print(#function, "\(addresses.count) addresses found")
+                    print(#function, "name : \(String(describing: addresses.last?.name))")
+                    for address in addresses {
+                        print(#function, "name : \(String(describing: address.name))")
+                        print(#function, "street number : \(String(describing: address.subThoroughfare))")
+                        print(#function, "street name : \(String(describing: address.thoroughfare))")
+                        print(#function, "city : \(String(describing: address.locality))")
+                        print(#function, "postal code : \(String(describing: address.postalCode))")
+                        print(#function, "province : \(String(describing: address.administrativeArea))")
+                        print(#function, "country : \(String(describing: address.country))")
+                        print(#function, "country code : \(String(describing: address.isoCountryCode))")
+                    }
+                    let address = addresses.last
+                    var resultAddress = "NA"
+                    if (address != nil){
+                        resultAddress = "resultAddress : \n\(address!.subThoroughfare ?? "NA") \(address!.thoroughfare ?? "NA"), \n\(address!.locality ?? "NA"). \(address!.postalCode ?? "NA")"
+                    }
+                    print(#function, "resultAddress : \(resultAddress)")
+                    completionHandler(resultAddress, nil)
+                } else {
+                    completionHandler("No addresses found", nil)
+                }
+            }
+        })
+    }
+    
+    
+    
 }
